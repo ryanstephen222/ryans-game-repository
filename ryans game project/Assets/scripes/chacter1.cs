c3398729a84d4788273975fb34f71ec28259eb99
ryanstephen222 <79139056+ryanstephen222@users.noreply.github.com>
2021-03-09 10:57:14 +0000

    tested script

---
+++ ryans game project/Assets/scripes/chacter1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chacter1 : MonoBehaviour
+{
+    //this is edit
+    public string startMessage; 
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(startMessage);
+    }
+
+    //this is edit
+    public string updateMessage;
+   
+    // Update is called once per frame
+    void Update()
+    {
+    Debug.Log(updateMessage);
+    }
+}
