7efaf490f097d034e93d050a56aa0c1e788e3968
ryanstephen222 <79139056+ryanstephen222@users.noreply.github.com>
2021-03-16 11:03:14 +0000

    added collision and movement
    can move can collided the ground is hard

---
+++ ryans game project/Assets/scripes/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float movementforce = 10f;
+
+    public float jumpforce = 1000f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.D) == true)
+        {
+            Rigidbody2D ourRigidbody = GetComponent<Rigidbody2D>();
+
+
+            ourRigidbody.AddForce(Vector2.right * movementforce);
+        }
+
+        if (Input.GetKey(KeyCode.A) == true)
+        {
+            Rigidbody2D ourRigidbody = GetComponent<Rigidbody2D>();
+
+
+            ourRigidbody.AddForce(Vector2.left * movementforce);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            Rigidbody2D ourRigidbody = GetComponent<Rigidbody2D>();
+
+
+            ourRigidbody.AddForce(Vector2.up * jumpforce);
+        }
+
+
+
+    }
+
+
+
+}
